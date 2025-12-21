00d7885f8557d6d44f6f9b79e55ac49717a52033
Daniel Stamer-Squair <daniel.robert.s-s@hotmail.com>
2025-12-21 23:46:42 +1100

    move function location

--- multitable/__init__.py
+++ multitable/__init__.py
@@ -14,7 +14,7 @@ from .frame_check import FrameTypeVerifier
 
 # CONSTANT PARAMETERS FOR MODULE
 
-module_version = "1.0.5"
+module_version = "1.0.5.1"
 meta_version = "1.0"
 """str: The version number of this module.
 Used to track compatibility and changes across releases.
@@ -30,3 +30,43 @@ def expected_meta_version(this_version:str) -> bool:
         print(f"DAG Code expects meta version of: {meta_version}")
     
     return cnd_match
+
+
+def concatlist(frames:list[MultiTable], engine:str) -> MultiTable:
+    """
+    Concat a list of multitable frames
+
+    Args:
+        frames (list[MultiTable]): The input list of MultiTable instances to concatenate.
+        engine (str): The engine type ("pandas", "polars", "pyspark").
+
+    Raises:
+        ValueError: If the list of frames is empty or if an unsupported engine is specified.
+        NotImplementedError: If the specified engine is not implemented.
+
+    Returns:
+        MultiTable: A new MultiTable instance containing the concatenated data.
+    """
+    if not frames:
+        raise ValueError("No frames to concatenate")
+    native_frames = [f.df for f in frames]
+
+    if engine == "pandas":
+        combined = pd.concat(native_frames, ignore_index=True)
+
+    elif engine == "polars":
+        combined = pl.concat(native_frames)
+
+    elif engine == "pyspark":
+        # Safe union across all frames
+        if len(native_frames) == 1:
+            combined = native_frames[0]
+        else:
+            combined = reduce(lambda df1, df2: df1.union(df2), native_frames)
+
+    else:
+        raise NotImplementedError(
+            f"RS400 Metaframe appendage not implemented for backend '{engine}'"
+        )
+    
+    return MultiTable(combined, src_path=frames[0].src_path, table_name=frames[0].table_name, frame_type=engine)
