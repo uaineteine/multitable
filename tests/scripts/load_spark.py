281fab3f2699f66584023f6dbe645064591490a3
Daniel Stamer-Squair <daniel.robert.s-s@hotmail.com>
2025-12-24 10:35:55 +1100

    fix tests

--- tests/scripts/load_spark.py
+++ tests/scripts/load_spark.py
@@ -37,7 +37,7 @@ for t in test_tables:
     #apply test of trimming
     print("Testing trimming of string columns...")
     for col in mt.columns:
-        mt.trimwhite()
+        mt.trimwhite(col)
     mt.show()
 
     #test some basic functionality
